be69ec2df07470729cff72a8e72b74e92210997e
CharuSachindu <charusachindu@gmail.com>
2025-08-03 01:01:04 +0530

    mobile nav bar

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,11 @@
 import Link from "next/link"
 import { Button } from "./ui/button"
 import Nav from "./Nav"
+import MobileNav from "./MobileNav"
 
 const Header = () => {
     return (
-        <header className="py=8 xl:py-12 text-white">
+        <header className="py=8 py-6 xl:py-12 text-white">
             <div className="container mx-auto flex justify-between items-center">
                 <Link href="/">
                 <h1 className="text-4xl font-semibold">
@@ -14,7 +15,7 @@ const Header = () => {
 
 
                 {/* Desktop Nav */}
-                <div className="hidden xl:flex items-center gap-8">
+                <div className="hidden sm:flex items-center gap-8">
                     <Nav />
                     {/* <Link href="/contact">
                     <Button>Hire me</Button>
@@ -23,8 +24,8 @@ const Header = () => {
 
                 {/* Mobile Nav  */}
                 
-                <div className="xl:hidden">
-                    Mobile Nav
+                <div className="sm:hidden  ">
+                    <MobileNav />
                 </div>
 
 
